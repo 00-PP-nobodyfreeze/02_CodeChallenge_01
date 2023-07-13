df3a2d4be2f8d82099421f4bc4dff9609696ed7e
nobodyfreeze <office.fherzog@gmail.com>
2023-07-13 14:32:44 +0200

    challenge solution 1

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,14 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+// Zahl 1 eingeben
+var zahl1 = parseFloat(prompt("Geben Sie die 1. Zahl ein:"));
+
+// Zahl 2 eingeben
+var zahl2 = parseFloat(prompt("Geben Sie die 2. Zahl ein:"));
+
+// Summe berechnen
+var summe = zahl1 + zahl2;
+
+// Summe in die Konsole ausgeben
+console.log("Die Summe der Zahlen ist: " + summe);
